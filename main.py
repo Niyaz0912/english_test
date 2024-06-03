b0c11e4d38cf5ea39a8d73e8e24d7675b8563276
Niyaz Khasanov <khasanchik85@gmail.com>
2024-06-04 00:24:16 +0500

    Revert "Написана функция get_result и докстринг"
    This reverts commit 9141704b0753b4237bb866f7e9f42b8daf8f6ba0.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.utils import get_data, get_user_level, base_program, get_result
+from utils.utils import get_data, get_user_level, base_program
 
 questions, levels = get_data('questions_data/questions.json')
 questions_list = questions['questions']
@@ -7,4 +7,3 @@ user_name = input("Введите Ваше имя: ")
 user_level = input("Выберите уровень сложности легкий/средний/сложный: ")
 user_questions = get_user_level(user_level, questions_list)
 user_answers = base_program(user_questions)
-user_result = get_result(user_answers, knowledge_level)
