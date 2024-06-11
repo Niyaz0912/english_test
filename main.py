39a6e0872765717cafd77039258bc4ce8fc53b0f
Niyaz Khasanov <khasanchik85@gmail.com>
2024-06-11 15:39:22 +0500

    Были исправлены ошибки в файле main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from utils.utils import get_data, get_user_level, base_program, get_result, write_answers
 
-questions, levels = get_data('questions_data/questions.json')
+questions, levels = get_data(r'questions_data/questions.json')
 questions_list = questions['questions']
-knowledge_level = levels ['levels']
-user_name = input("Введите Ваше имя: ")
-user_level = input("Выберите уровень сложности легкий/средний/сложный: ")
-user_questions = get_user_level(user_level, questions_list)
-user_answers = base_program(user_questions)
+knowledge_level = levels['levels']
+user_name = input("Введите свое имя: ")
+user_level = input("Введите уровень сложности легкий/средний/сложный: ")
+user_words = get_user_level(user_level, questions_list)
+user_answers = base_program(user_words)
 user_result = get_result(user_answers, knowledge_level)
-print(write_answers(fr'sudents_data/{user_name}_answers.json', user_answers))
+print(f"\nВаш ранг:\n{user_result}")
+write_answers(fr'students_data/{user_name}_answers.json', user_answers)
